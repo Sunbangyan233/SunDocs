04d882ecbeb0e2e70336016c020db3b7ab390fe4
SunRestikX <110176166+Sunbangyan233@users.noreply.github.com>
2026-05-15 11:06:08 +0800

    ignore brokenlinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,6 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 
 /** @type {import('@docusaurus/types').Config} */
@@ -15,19 +9,13 @@ const config = {
   tagline: 'All in one for documents',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://app.sunimg.top',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Sunbangyan233', // Usually your GitHub org/user name.
-  projectName: 'SunDocs', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'Sunbangyan233', 
+  projectName: 'SunDocs', 
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -77,7 +65,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'aio-SunDocs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -120,10 +108,6 @@ const config = {
                 label: '我们的 Bilibili',
                 to: 'https://space.bilibili.com/599959906',
               },         
-              {
-                label: '我们的Telegram',
-                to: 'https://t.me/SunRestik',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/Sunbangyan233/SunDocs',
